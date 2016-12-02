616703c8decd826d25b44d484f252c792521f493
kalbuth <kalbuth@gmail.com>
2016-12-02 02:19:13 +0100

    Signed-off-by: kalbuth <kalbuth@gmail.com>

--- Moose_Failystan.lua
+++ Moose_Failystan.lua
@@ -22,7 +22,7 @@ end
 -- Faily_CC : prise en charge des missions
 
 FAILY_COMMANDCENTER = {
-	ClassName = "FAILY_COMMANDCENTER"
+	ClassName = "FAILY_COMMANDCENTER",
 }
 
 function FAILY_COMMANDCENTER:New( HQ, HQName )
@@ -53,7 +53,7 @@ end
 failystan = {}
 
 FAILY_MISSION = {
-	ClassName = "FAILY_MISSION"
+	ClassName = "FAILY_MISSION",
 }
 
 function FAILY_MISSION:New(HQ, MissionName, MissionPriority, MissionBriefing, MissionCoalition )
@@ -67,7 +67,7 @@ function FAILY_MISSION:New(HQ, MissionName, MissionPriority, MissionBriefing, Mi
 end
 	
 failystan.MISSION_TRANSPORT = {
-	ClassName = "MISSION_TRANSPORT"
+	ClassName = "MISSION_TRANSPORT",
 }
 
 function failystan.MISSION_TRANSPORT:New( HQ, MissionName, MissionPriority, MissionBriefing, MissionCoalition, TransportGroup, TransportDest )
@@ -82,21 +82,72 @@ end
 
 do
 	DETECTION_GCI = {
-		ClassName = DETECTION_ZONE
-		DetectionZone = {}
+		ClassName = DETECTION_ZONE,
+		DetectionZone = {},
 	}
-	function DETECTION_GCI:New(DetectionSetGroup, DetectionRange, DetectionZone)
+	function DETECTION_GCI:New(DetectionSetGroup, DetectionRange, DetectionZone, Mission, CommandCenter, Frequency)
 		-- Inherits from DETECTION_BASE
 		local self = BASE:Inherit( self, DETECTION_BASE:New( DetectionSetGroup, DetectionRange ) )
 		
 		self.DetectionZone = DetectionZone
+		self.Mission = Mission
+		self.CommandCenter = CommandCenter
+		self.Frequency = Frequency
+		
+		self:SetFrequency( Frequency )
+		self:SetCallsign( 101 )
+		self:_InitRadio()
+		
 		self.Schedule(0, 30)
 		
 		return self
 	end
 	
+	function DETECTION_GCI:SetFrequency(Frequency)
+		-- sets the emitter frequency for GCI Command Center
+		self:F2( Frequency )
+		local SetFrequency = {
+			['id'] = 'SetFrequency',
+			['params'] = {
+				['frequency'] = Frequency,
+				['modulation'] = 0,
+			},
+		}
+		self.CommandCenter:SetCommand( SetFrequency )
+	end
 
-
+	function DETECTION_GCI:SetCallsign( Callsign )
+		-- sets the emitter frequency for GCI Command Center
+		self:F2( Callsign )
+		local SetCallsign = {
+			['id'] = 'SetCallsign',
+			['params'] = {
+				['callname'] = Callsign,
+				['number'] = 1,
+			},
+		}
+		self.CommandCenter:SetCommand( SetCallsign )
+	end
+	
+	function DETECTION_GCI:_InitRadio()
+		self:F2()
+		local coal = self.CommandCenter:GetCoalition()
+		if not self.MenuGCI then
+			self.MenuGCI = MENU_COALITION:New( coal, "GCI" )
+			self.MenuCmdPicture = MENU_COALITION_COMMAND:New( coal, "Request Picture", self.MenuGCI, self:GetPicture() )
+		end
+	end
+	
+	function DETECTION_GCI:GetPicture()
+		self:F2()
+		-- to be continued
+		-- DETECION_GCI:CreateDetectionSets() a faire
+		-- cf Detection.lua l. 796 & 905
+		
+		
+	end
+	
+	
 	FAILY_DETECTION_DISPATCHER = {
 		 ClassName = "FAILY_DETECTION_DISPATCHER",
 		 Mission = nil,
